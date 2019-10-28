d14fdc59d298c6b21473058a92bd9ffdbebf7d0c
Amit Kumar <fbpass.res14@gmail.com>
2019-10-28 13:34:42 +0530

    Add license header and rename package name to com.audical.app

--- app/src/androidTest/java/com/audical/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/audical/app/ExampleInstrumentedTest.kt
@@ -15,13 +15,23 @@
  */
 package com.audical.app
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.audical.app", appContext.packageName)
     }
 }
--- app/src/test/java/com/audical/app/ExampleUnitTest.kt
+++ app/src/test/java/com/audical/app/ExampleUnitTest.kt
@@ -15,13 +15,17 @@
  */
 package com.audical.app
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
     }
 }
